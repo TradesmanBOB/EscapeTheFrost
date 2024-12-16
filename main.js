e04262c7034fdd5abbce91a1df9ed8499b101d10
Corrigana <rednaxc1@gmail.com>
2024-12-16 08:13:51 -0500

    Learning to fix button

--- main.js
+++ main.js
@@ -3,13 +3,11 @@
 // Nav Open/Closer for Mobile
 
 // Toggle function for opening and closing the menu
-function toggleMenu() {
-    const navMenu = document.querySelector('.nav-menu');
-  
-    // Check if the menu is open or closed
-    if (navMenu.style.left === '-250px') {
-      navMenu.style.left = '0';  // Show the menu
-    } else {
-      navMenu.style.left = '-250px';  // Hide the menu
-    }
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
   }
+}
